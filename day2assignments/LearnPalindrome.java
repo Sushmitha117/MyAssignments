3365bacd2611c53dc5b6b69ba369c862630355ca
Sushmitha117 <165500422+Sushmitha117@users.noreply.github.com>
2024-04-04 18:22:37 +0530

    Add files via upload
    week1.day2 assignments

---
+++ day2assignments/LearnPalindrome.java
@@ -0,0 +1,30 @@
+package week1.day2assignments;
+
+public class LearnPalindrome {
+
+	public static void main(String[] args) {
+		
+		
+		int number=313 ;
+		int number2= number;
+		int rem;
+		int reverseNum=0;
+	while(number>0)	
+	{
+		rem=number%10;   
+		reverseNum=(reverseNum*10)+rem;
+		number=number/10;  
+		
+	}
+	
+	if(number2==reverseNum)
+	{
+		System.out.println("the number is Palindrome");
+	}
+	else
+	{
+		System.out.println("the number is not Palindrome");
+	}
+	}
+	
+}
