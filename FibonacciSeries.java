08d2cd742ce4798284552ff34229d65626c0177a
Sushmitha117 <165500422+Sushmitha117@users.noreply.github.com>
2024-04-04 18:14:33 +0530

    Add files via upload
    week1/day2

---
+++ FibonacciSeries.java
@@ -0,0 +1,20 @@
+package week1.day2;
+
+public class FibonacciSeries {
+
+	public static void main(String[] args) {
+
+int n = 8, t1 = 0, t2 = 1;
+System.out.println("Fibonacci Series of " + n + " terms:");
+
+for (int i = 0; i < n;i++) {
+	System.out.println(t1);
+	
+int sum = t1 + t2;
+t1 = t2;
+t2 = sum;
+
+}}
+}
+
+
---
+++ IsPrimeNumber.java
@@ -0,0 +1,29 @@
+package week1.day2;
+
+public class IsPrimeNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int n=37;
+		int count=0;
+		for (int i=2;i<n;i++)
+		{
+			if(n%i==0)
+			{
+				count++;
+			}
+		}
+			if(count==0)
+			{
+				System.out.println("the number is Prime number");
+			}
+			else
+			{
+				System.out.println("the number is not a Prime number");
+			}
+			}
+				
+		}
+	
+
